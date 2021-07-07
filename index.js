21c8e4ee91ef4eec7e90d0d31b91a675a65aa245
AlexTremoureux <alex.tremoureux@gmail.com>
2021-07-07 23:21:41 +0200

    insertion des articles pour chaque photographe en javascript à partir du fichier json

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
+//Constantes
 const container = document.getElementById("container");
-/*const name = document.getElementById("name");
-const id = document.getElementById("id");
-const city = document.getElementById("city");
-const country = document.getElementById("country");
-const tags = document.getElementById("tags");
-const tagline = document.getElementById("tagline");
-const price = document.getElementById("price");
-const portrait = document.getElementById("portrait");*/
+const nav = document.getElementById("nav");
+const requestURL = 'https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json';
 
-let requestURL = 'https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json';
+//Fonction getPhotograph venant fetch les données et remplir les articles
 const getPhotograph = async function () {
   let response = await fetch(requestURL)
   let data = await response.json()
-  console.log(data.photographers)
+  //compteur pour définir les id = taglist+compteur de la liste de tags
+  let countId = 0;
+  //Boucle sur chaque photographe afin de lui créer son propre article
   data.photographers.forEach(element => {
-    
-   container.innerHTML += 
-   `<article>
+    countId ++;
+    container.innerHTML += 
+    `<article>
       <section class="headerSection">
         <a href="">
           <img id="portrait" src="./Photos/Medias/Sample Photos/Photographers ID Photos/${element.portrait}" alt="">
@@ -29,16 +26,20 @@ const getPhotograph = async function () {
         <p id="price">${element.price}€/jour</p>
       </section>
       <section class="tagSection">
-        <ul>
-          <li id="tags"><a>#${element.tags}</a></li>
+        <ul id="tagList${countId}">
         </ul>
       </section>
     </article>`;
-  
-});
+    //Boucle forEach afin d'afficher les différents tags de chaque photographe
+    let tagList = document.getElementById("tagList"+countId);
+    [...element.tags].forEach(tagsElement => {
+      tagList.innerHTML += 
+      `<li id="tags"><a>#${tagsElement}</a></li>`
+    });
+  });
 }
+
+//Calcul du temps d'execution de la fonction
 console.time('Execution Time');
 getPhotograph()
 console.timeEnd('Execution Time');
-
-
